f21c623637b2d1d5393fb2131e2484d934079407
Mholy <5146835+Mholy@users.noreply.github.com>
2024-09-16 13:56:00 +0400

    Revert "feat: Add global function to retrieve the current directory for oil.nvim winbar"
    This reverts commit 321fae57df666e56f9717c906de2b74890c895ce.

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -112,17 +112,6 @@ return {
     "stevearc/oil.nvim",
     lazy = false,
     config = function()
-      -- Declare a global function to retrieve the current directory
-      function _G.get_oil_winbar()
-        local dir = require("oil").get_current_dir()
-        if dir then
-          return vim.fn.fnamemodify(dir, ":~")
-        else
-          -- If there is no current directory (e.g. over ssh), just show the buffer name
-          return vim.api.nvim_buf_get_name(0)
-        end
-      end
-
       require("oil").setup {
         skip_confirm_for_simple_edits = true,
         keymaps = {
@@ -153,9 +142,6 @@ return {
         },
         float = {
           border = "single",
-        },
-        win_options = {
-          winbar = "%!v:lua.get_oil_winbar()",
         },
       }
     end,
