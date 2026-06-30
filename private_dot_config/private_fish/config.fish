43eec5b8830644d328f43cc5ad7c1570ce9e0d39
mholy <5146835+Mholy@users.noreply.github.com>
2026-06-30 18:21:17 +0400

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,10 +2,11 @@ set -gx EDITOR hx
 
 if status is-interactive
     fzf --fish | source
-    zoxide init --cmd j fish | source
+    zoxide init fish --cmd j | source
     keychain --eval --quiet --confallhosts | source
     alias ls='eza --group-directories-first'
     alias ll='eza -lh --group-directories-first'
     alias la='eza -lah --group-directories-first'
     alias cat='bat'
+    alias lg='lazygit'
 end
